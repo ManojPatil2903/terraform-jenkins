2182ff30724a571c779c16a010c003294326d4bb
ManojPatil2903 <145556779+ManojPatil2903@users.noreply.github.com>
2024-04-13 11:10:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
-
-region = ap-south-1
+    region = "ap-south-1"
   
 }
-resource "aws_instance" "ec2-1" {
-  
-  ami = "ami-09298640a92b2d12c"
-  instance_type = "t2.micro"
-  key_name = "comman"
-  security_groups = [ "Manoj-SG" ]
-  
+resource "aws_instance" "my-vm" {
 
+    ami = "ami-09298640a92b2d12c"
+    instance_type = "t2.micro"
+    key_name = "comman"
+    security_groups = [ "Manoj-SG" ]
+  
 }
